0f152a49ec26905d16a4bb669152e159a924762c
christianivh <christianivh@hotmail.com>
2020-05-02 13:11:42 -0500

    fixing2.1

--- hadoop/java_files/LineCount.java
+++ hadoop/java_files/LineCount.java
@@ -26,6 +26,7 @@ public class LineCount {
 		public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) 
 		throws IOException {
 			int sum = 0;
+			
 			while (values.hasNext()) {
 				sum += values.next().get();
 			}
