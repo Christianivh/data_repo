9a036eb14fd2192ac2d97ec351f6181e868ea57b
christianivh <christianivh@hotmail.com>
2020-05-02 13:01:19 -0500

    fixing2

--- hadoop/java_files/LineCount.java
+++ hadoop/java_files/LineCount.java
@@ -9,29 +9,29 @@ import org.apache.hadoop.util.*;
 
 public class LineCount {
 
-		public static class Map extends MapReduceBase implements 
-		Mapper<LongWritable, Text, Text, IntWritable> {
-			private final static IntWritable uno= new IntWritable(1);
-			private Text word = new Text("Total Lineas");
+	public static class Map extends MapReduceBase implements
+	Mapper<LongWritable, Text, Text, IntWritable> {
+		private final static IntWritable uno= new IntWritable(1);
+		private Text word = new Text("Total Lineas");
 
-			public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) 
-			throws IOException { 
-				output.collect(word, uno);
-			}
+		public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter)
+		throws IOException { 
+			output.collect(word, uno);
 		}
+	}
 
-		public static class Reduce extends MapReduceBase implements
-		Reducer<Text, IntWritable, Text, IntWritable> {
+	public static class Reduce extends MapReduceBase implements
+	Reducer<Text, IntWritable, Text, IntWritable> {
 
-			public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) 
-			throws IOException {
-				int sum = 0;
-				while (values.hasNext()) {
-					sum += values.next().get();
-				}
-				output.collect(key, new IntWritable(sum));
+		public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) 
+		throws IOException {
+			int sum = 0;
+			while (values.hasNext()) {
+				sum += values.next().get();
 			}
+			output.collect(key, new IntWritable(sum));
 		}
+	}
 
 	public static void main(String[] args) throws Exception {
 		JobConf conf = new JobConf(LineCount.class);
