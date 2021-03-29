b578b02284a308dbd85edf67ef5278e4712909d6
christianivh <christianivh@hotmail.com>
2021-03-28 21:38:57 -0500

    fix reduce

--- hadoop/reducer.py
+++ hadoop/reducer.py
@@ -9,7 +9,7 @@ for line in sys.stdin:
 	# remove leading and trailing whitespace 
 	line = line.strip()
 	# parse the input we got from mapper.py word, 
-	count = line.split('\t')[1]
+	word, count = line.split('\t', 1)
 	# convert count (currently a string) to int 
 	try:
 		count = int(count) 
